f2cd2ba7f9ed66cc2bb6142a79f282cde0430926
Melhem El Dirani <melhemdirani@Melhems-MacBook-Air.local>
2021-10-02 20:29:59 +0300

    adding database connection

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db= knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-cylindrical-33554',
-      user : 'melhemdirani',
-      password : '',
-      database : 'smart-brain'
+      host : process.env.DATABASE_URL,
+      ssl: true
     }
 });
 
